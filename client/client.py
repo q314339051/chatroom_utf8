f816eac50d7e773bbee89063beef32b6d58f4cdd
Jay <linyj_work@163.com>
2019-04-26 21:30:22 +0800

    add file

---
+++ client/client.py
@@ -0,0 +1,6 @@
+# -*- coding:utf-8 -*-
+"""
+    chatroom project for client
+    env:python3.5
+    exc:for socket and Procrss and Thread
+"""
---
+++ server/server.py
@@ -0,0 +1,6 @@
+# -*- coding:utf-8 -*-
+"""
+    chatroom project for server
+    env:python3.5
+    exc:for socket and Procrss and Thread
+"""
