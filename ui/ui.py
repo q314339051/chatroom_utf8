7ad29168b0f75e6e9887e31087219433a1559c62
q314339051 <49966733+q314339051@users.noreply.github.com>
2019-05-10 21:06:05 +0800

    1

--- ui/ui.py
+++ ui/ui.py
@@ -124,7 +124,9 @@ class MainWindow:
         treeF2_1 = tree.insert(treeF2, 0, text="中国黑龙江", values=("中国黑龙江"))
         tree.bind("<Double-1>", self.dblclickAdaptor(self.dblclick, tree=tree))
 
-        Button(self.root, text="添加好友", command=self.add_friend).place(x=170, y=430)
+        Button(self.root, text="添加好友", command=self.add_friend).place(x=30, y=430)
+        Button(self.root, text="创建群", command=None).place(x=110, y=430)
+        Button(self.root, text="加入群", command=self.join_group).place(x=180, y=430)
 
     def dblclickAdaptor(self, fun, **kwds):
         return lambda event, fun=fun, kwds=kwds: fun(event, **kwds)
@@ -138,17 +140,22 @@ class MainWindow:
         # print("you clicked on ", tree.item(item, "values"))
         # print(tree.item(item, "values")[0])
 
-    def chat_window(self, name):  # 创建聊天窗口
+    def window_exist(self,name): # 判断窗口是否已创建
         # 如果该窗口存在，则不创建
         if name in self.dict:
             # 显示已经打开窗口
             self.dict[name].deiconify()
+            return True
+    def chat_window(self, name):  # 创建聊天窗口
+        # 判断窗口是否已存在
+        if self.window_exist(name):
+            # 窗口存在则返回
             return
         # 创建聊天窗口
         self.chat = Toplevel()
         self.dict[name] = self.chat
         # 窗口大小
-        self.chat.geometry("500x400")
+        self.chat.geometry("500x430")
         # 设置窗口大小固定
         self.chat.resizable(0, 0)
         # 设置窗口标题
@@ -163,37 +170,66 @@ class MainWindow:
         b.pack()
         text2 = Text(self.chat, )
         text2.place(in_=b, width=500, height=100)
+        # 发送按钮
+        Button(self.chat, text="发送", command=lambda: self.recv_message(text1,text2.get("1.0",END))).place(x=450, y=400)
         # 更新聊天信息
-        self.recv_message(text1)
+        self.recv_message(text1,"3546843")
 
         # 点击关闭按钮触发事件
-        self.chat.protocol("WM_DELETE_WINDOW", lambda: self.on_closing(name=name))
+        self.chat.protocol("WM_DELETE_WINDOW", lambda: self.close_window(name=name))
         self.chat.mainloop()
 
-    def recv_message(self, text):  # 接收信息
+    def recv_message(self, text,msg):  # 接收信息
         text.config(state=NORMAL)
         # 显示信息
-        text.insert(END, "5143543")
+        text.insert(END, msg)
         text.config(state=DISABLED)
 
-    def on_closing(self, name):  # 关闭聊天窗口
+    def close_window(self, name):  # 关闭窗口
         self.dict[name].destroy()
         del self.dict[name]
 
     def add_friend(self):
+        # 判断窗口是否已存在
+        if self.window_exist("添加好友"):
+            # 窗口存在则返回
+            return
         # 创建加好友窗口
         self.af = Toplevel()
         # 窗口大小
-        self.af.geometry("500x200")
+        self.af.geometry("400x130")
         # 设置窗口大小固定
         self.af.resizable(0, 0)
         # 设置窗口标题
         self.af.title("添加好友")
-        Label(self.af, text='输入好友账号').place(x=50, y=30)
+        self.dict["添加好友"] = self.af
+        Label(self.af, text='输入好友账号').place(x=50, y=40)
         text1 = StringVar()
-        Entry(self.af, textvariable=text1).place(x=130, y=30)
-        Button(self.af, text="添加好友", command=None).place(x=300, y=30)
-        Button(self.af, text="加入群", command=None).place(x=380, y=30)
+        Entry(self.af, textvariable=text1).place(x=130, y=40)
+        Button(self.af, text="添加好友", command=None).place(x=300, y=40)
+        # 点击关闭按钮触发事件
+        self.af.protocol("WM_DELETE_WINDOW", lambda: self.close_window(name="添加好友"))
+
+    def join_group(self):
+        # 判断窗口是否已存在
+        if self.window_exist("加入群聊"):
+            # 窗口存在则返回
+            return
+        # 创建加群窗口
+        self.af = Toplevel()
+        # 窗口大小
+        self.af.geometry("400x130")
+        # 设置窗口大小固定
+        self.af.resizable(0, 0)
+        # 设置窗口标题
+        self.af.title("加入群聊")
+        self.dict["加入群聊"] = self.af
+        Label(self.af, text='输入群号').place(x=50, y=40)
+        text1 = StringVar()
+        Entry(self.af, textvariable=text1).place(x=130, y=40)
+        Button(self.af, text="加入群聊", command=None).place(x=300, y=40)
+        # 点击关闭按钮触发事件
+        self.af.protocol("WM_DELETE_WINDOW", lambda: self.close_window(name="加入群聊"))
 
 
 if __name__ == '__main__':
